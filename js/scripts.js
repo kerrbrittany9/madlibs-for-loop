0b1620dee678d226d28f63cd73d23ba27a70f522
jacob ruleaux and max scher <max@>
2017-05-31 15:12:27 -0700

    add js form

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
+    var businessNameInput = $("input#businessName").val();
+    var contactNameInput = $("input#contactName").val();
+    var addressInput = $("input#address").val();
+    var emailInput = $("input#email").val();
+    var subjectInput = $("input#subject").val();
 
+    $(".contactName").text(contactNameInput);
+    $(".businessName").text(businessNameInput);
+    $(".email").text(emailInput);
+    $(".address").text(addressInput);
+    $(".subject").text(subjectInput);
 
+    $("#letter").show();
 
-
-  $("#letter").show();
-  
-  event.preventDefault();
+    event.preventDefault();
   });
 });
